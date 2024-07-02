b8f4759f5470f1d2e5bcd24e4e74b094ac1bf701
nesirid <nasirid@code.edu.az>
2024-07-03 02:05:50 +0400

    FirstCommitCompliteTask

---
+++ Service/Services/Interfaces/ICityService.cs
@@ -0,0 +1,15 @@
+﻿using Service.DTOs.Admin.Cities;
+
+namespace Service.Services.Interfaces
+{
+    public interface ICityService
+    {
+        Task<IEnumerable<CityDto>> GetAllAsync();
+        Task<CityDto> GetByIdAsync(int id);
+        Task<CityDto> GetByNameAsync(string name);
+        Task CreateAsync(CityCreateDto model);
+        Task<IEnumerable<CityDto>> FilterAsync(string name, string countryName);
+        Task EditAsync(int id, CityEditDto model);
+        Task DeleteAsync(int id);
+    }
+}
